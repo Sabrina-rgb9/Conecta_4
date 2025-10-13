10e6f35d4af5504083a6766ce61f58e4c1e9d76a
Christopher Carrillo <carrillocrespo17@gmail.com>
2025-10-13 17:43:41 +0200

    Base projecte

---
+++ Projecte/src/main/java/com/project/Main.java
@@ -0,0 +1,38 @@
+package com.project;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    final int WINDOW_WIDTH = 340;
+    final int WINDOW_HEIGHT = 435;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        // Carrega la vista inicial des del fitxer FXML
+        Parent root = FXMLLoader.load(getClass().getResource("/assets/")); //Poner el nombre del fxml
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.setTitle("Conecta 4");
+        stage.setWidth(WINDOW_WIDTH);
+        stage.setHeight(WINDOW_HEIGHT);
+        stage.show();
+
+        // Afegeix una icona només si no és un Mac
+        if (!System.getProperty("os.name").contains("Mac")) {
+            Image icon = new Image("file:icons/icon.png");
+            stage.getIcons().add(icon);
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
