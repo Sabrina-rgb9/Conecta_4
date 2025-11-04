0c3feeb1b720f1eae7c3278471d44322a58f0d34
Christopher Carrillo <carrillocrespo17@gmail.com>
2025-11-05 00:11:28 +0100

    Conecta 4 Done

--- Projecte_3.0/src/main/java/com/clientFX/Main.java
+++ Projecte_3.0/src/main/java/com/clientFX/Main.java
@@ -45,8 +45,8 @@ public class Main extends Application {
         
         stage.setScene(scene);
         stage.setTitle("Conecta 4");
-        stage.setMinWidth(800);
-        stage.setMinHeight(600);
+        stage.setMinWidth(900);
+        stage.setMinHeight(700);
         stage.show();
         
         // Ir a vista de configuración inicial
